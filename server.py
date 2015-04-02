26afc6de22148aa4df54c6c0b2e2b7c7c0d8b470
jhaip <jhaip@mit.edu>
2015-04-02 15:50:01 -0400

    static images and better instructions

--- server.py
+++ server.py
@@ -2,23 +2,42 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 from botchallenge import *
+import os
+import sys
 
-robot = Robot("jhaip","localhost")
+robot = None
+robot_ready = False
+
+if len(sys.argv) == 2:
+    MINECRAFT_USERNAME = str(sys.argv[1])
+    MINECRAFT_SERVER = "localhost"
+    try:
+        robot = Robot(MINECRAFT_USERNAME, MINECRAFT_SERVER)
+    except:
+        print("")
+        print("WARNING: Could not connect to Minecraft server")
+        print("Did you really want to connect as "+MINECRAFT_USERNAME+" to the server at "+MINECRAFT_SERVER+"?")
+        print("And make sure the Modified Bukkit Server is running!")
+    else:
+        robot_ready = True
+        print("SUCCESS: Connected to Minecraft Server at "+MINECRAFT_SERVER+" with username "+MINECRAFT_USERNAME)
+else:
+    print("No Minecraft username given - skipping connection to server")
 
 
 class Hello(tornado.websocket.WebSocketHandler):
     def open(self):
-        self.write_message("Hello, world")
-        print("WROTE MESSAGE")
+        self.write_message("Hello web page")
+        print("Said hello to the web page")
 
     def on_message(self, message):
-        print("RECEIVED MESSAGE")
-        print(message)
-        robot.message_all(message)
-        if "down" in message:
-            robot.move(Dir.DOWN)
-        if "up" in message:
-            robot.move(Dir.UP)
+        print("Received message: '"+message+"'")
+        if robot_ready:
+            robot.message_all(message)
+            if "down" in message:
+                robot.move(Dir.DOWN)
+            if "up" in message:
+                robot.move(Dir.UP)
 
     def on_close(self):
         print("CLOSING SERVER")
@@ -31,13 +50,18 @@ class Main(tornado.web.RequestHandler):
 
 class Application(tornado.web.Application):
     def __init__(self):
+        settings = {
+            'debug': True, 
+            'static_path': os.path.join(os.path.dirname(__file__), 'static')
+        }
         handlers = [
             (r"/", Main),
             (r"/websocket", Hello),
         ]
-        tornado.web.Application.__init__(self, handlers)
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 if __name__ == "__main__":
     app = Application()
     app.listen(8888)
+    print("Starting Tornado Server at localhost:8888")
     tornado.ioloop.IOLoop.instance().start()
