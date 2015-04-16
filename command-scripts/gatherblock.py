a71bda7b304fb84501ee2ae8b789e9a58ebbee88
jhaip <jhaip@mit.edu>
2015-04-16 19:30:37 -0400

    smarter collecting

--- command-scripts/gatherblock.py
+++ command-scripts/gatherblock.py
@@ -6,27 +6,37 @@ import sys
 
 def gather_block(robot, TARGET_LIST):
     print("*** STARTING GATHER_BLOCK SCRIPT")
+    startLocation = robot.get_location()
 
     locations = []
     for t in TARGET_LIST:
         locations += robot.find_type_nearby(t)
     print(locations)
 
+    def coordDist(x):
+        return int(x.distance(startLocation))
+
+    locations.sort(key=coordDist)
+
     limit1 = 0
-    while len(locations) > 0 and limit1 < 20:
+    while len(locations) > 0 and limit1 < 20000:
         limit1 += 1
         location = locations[0]
         block_type = robot.get_block_type(robot.get_location().direction(location))
-        limit2 = 0
-        while block_type not in TARGET_LIST and limit2 < 20:
-            limit2 += 1
+        pathfinding_locations = []
+        while block_type not in TARGET_LIST:
             robot.move(robot.find_path(location))
+            if len(pathfinding_locations) > 3 and pathfinding_locations[-2] == robot.get_location():
+                robot.message_owner("I got stuck so I gave up. :(")
+                return
+            pathfinding_locations.append(robot.get_location())
             robot.turn(robot.get_location().direction(location))
             block_type = robot.get_block_type(robot.get_location().direction(location))
         robot.mine(robot.get_location().direction(location))
         locations = []
         for t in TARGET_LIST:
             locations += robot.find_type_nearby(t)
+        locations.sort(key=coordDist)
 
     robot.message_all("I'm done gathering!")
     print("done!")
